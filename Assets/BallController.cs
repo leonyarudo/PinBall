d61fb8d013a53e9dd4b6e421d4672b500673cade
宇枝礼央 <leonyarudo@yahoo.co.jp>
2020-02-06 19:50:18 +0900

    Lesson5 3Dゲームアプリを作ろう Vol.1
    PinBallを作ったファイルです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+	private float visiblePosZ = -6.5f;
+
+	private GameObject gameoverText;
+
+	private GameObject PointText;
+
+	private int Point;
+
+	// Use this for initialization
+	void Start () {
+		this.gameoverText = GameObject.Find("GameOverText");
+		this.PointText = GameObject.Find("PointText");
+	}
+		
+	void Update () {
+		if (this.transform.position.z < this.visiblePosZ) {
+			this.gameoverText.GetComponent<Text> ().text = "Game Over";
+		}
+	}
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "SmallStarTag"){
+			Point += 5;
+		} else if (other.gameObject.tag == "LargeStarTag") {
+			Point += 10;
+		} else if (other.gameObject.tag == "SmallCloudTag") {
+			Point += 15;
+		} else if (other.gameObject.tag == "LargeCloudTag") {
+			Point += 20;
+		}
+		this.PointText.GetComponent<Text> ().text = Point + "point";
+	}
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+	private HingeJoint myHingeJoint;
+
+	private float defaultAngle = 20;
+	private float flickAngle = -20;
+
+		// Use this for initialization
+	void Start () {
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		SetAngle(this.defaultAngle);
+	}
+
+	public class TouchManager{
+		public bool _touch_flag;
+		public Vector2 _touch_position;
+
+	}
+	
+	// Update is called once per frame
+	void Update (){
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		for (int i = 0; i < Input.touchCount; i++) {
+			var pos = Input.touches[i].position;
+			if (pos.x >= Screen.width / 2 && tag == "RightFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (pos.x <= Screen.width / 2 && tag == "LeftFripperTag") {
+				SetAngle (this.flickAngle);
+			}
+			if (Input.GetTouch(i).phase == TouchPhase.Ended && pos.x <= Screen.width / 2 && tag == "LeftFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+			if (Input.GetTouch(i).phase == TouchPhase.Ended && pos.x >= Screen.width / 2 && tag == "RightFripperTag") {
+				SetAngle (this.defaultAngle);
+			}
+		}
+	/*	if (Input.GetMouseButtonDown (0) && Input.mousePosition.x >= Screen.width / 2 && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetMouseButtonDown (0) && Input.mousePosition.x <= Screen.width / 2 && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		if (Input.GetMouseButtonUp (0) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}					
+		if (Input.GetMouseButtonUp (0) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+*/
+	}
+	public void SetAngle (float angle){
+			JointSpring jointSpr = this.myHingeJoint.spring;
+			jointSpr.targetPosition = angle;
+			this.myHingeJoint.spring = jointSpr;
+	}
+}
